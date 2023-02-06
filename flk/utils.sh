93e2d3e065b29b32966bb42706872f9056cc48ab
Helder Oliveira <heldrida@gmail.com>
2023-02-06 17:18:12 +0000

    refactor: 💡 add remaining servers

--- flk/utils.sh
+++ flk/utils.sh
@@ -1,13 +1,22 @@
 # Common tasks
 
-alias ssh_id_helder_flk="ssh -i ~/.ssh/id_flk"
+alias ssh_id_flk="ssh -i ~/.ssh/id_flk"
 
 # Ssh into a server by running:
 # ssh_to <name>, e.g. ssh_to ubuntu_for_dx
 ssh_to() {
   case $1 in
     ubuntu_for_dx)
-        ssh_id_helder_flk "$FLK_NET_DO_UBUNTU_DX_USER_AT"
+        ssh_id_flk "$FLK_NET_DO_UBUNTU_DX_USER_AT"
+      ;;
+    debian_for_dx)
+        ssh_id_flk "$FLK_NET_DO_DEBIAN_DX_USER_AT"
+      ;;
+    centos_for_dx)
+        ssh_id_flk "$FLK_NET_DO_CENTOS_DX_USER_AT"
+      ;;
+    fedora_for_dx)
+        ssh_id_flk "$FLK_NET_DO_FEDORA_DX_USER_AT"
       ;;
   esac
 }
